454e2e08ac503ae2e34da85f2dd31fd8975241a0
mij88 <mijalshrestha88@gmail.com>
2024-08-05 17:08:46 -0400

    Spring Boot initial set up

---
+++ watchLog/src/test/java/com/watchLog/watchLog/WatchLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchLog.watchLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
