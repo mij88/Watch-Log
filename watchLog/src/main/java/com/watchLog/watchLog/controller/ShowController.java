0c00a3b9d2dc79ffd8d0ba82f84f099c8e12f114
mij88 <mijalshrestha88@gmail.com>
2024-08-09 15:43:45 -0400

    Added show register page to add new shows

--- watchLog/src/main/java/com/watchLog/watchLog/controller/ShowController.java
+++ watchLog/src/main/java/com/watchLog/watchLog/controller/ShowController.java
@@ -1,14 +1,31 @@
 package com.watchLog.watchLog.controller;
 
+import com.watchLog.watchLog.entity.Shows;
+import com.watchLog.watchLog.service.ShowService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ShowController {
 
+    @Autowired
+    private ShowService service;
 
     @GetMapping
-    public String home() {
-        return "home";
+    public String home() { return "home";}
+
+    @GetMapping("/show_register")
+    public String showRegister() {return "showRegister";}
+
+    @GetMapping("/show_list")
+    public String showList() {return "showList";}
+
+    @PostMapping("/save")
+    public String addShow(@ModelAttribute Shows s) {
+        service.save(s);
+        return "redirect:/show_list";
     }
 }
--- watchLog/src/main/java/com/watchLog/watchLog/service/ShowService.java
+++ watchLog/src/main/java/com/watchLog/watchLog/service/ShowService.java
@@ -1,5 +1,6 @@
-package com.watchLog.watchLog;
+package com.watchLog.watchLog.service;
 
+import com.watchLog.watchLog.entity.Shows;
 import com.watchLog.watchLog.repository.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,7 @@ public class ShowService {
 
     @Autowired
     private ShowRepository sRepo;
+
+    public void save(Shows s) {sRepo.save(s);}
 
 }
