6146d82c6027ba631e7e09b403b1933cdf37ad71
mij88 <mijalshrestha88@gmail.com>
2024-08-10 15:40:10 -0400

    Output added shows to Show List table

--- watchLog/src/main/java/com/watchLog/watchLog/controller/ShowController.java
+++ watchLog/src/main/java/com/watchLog/watchLog/controller/ShowController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ShowController {
@@ -21,7 +24,11 @@ public class ShowController {
     public String showRegister() {return "showRegister";}
 
     @GetMapping("/show_list")
-    public String showList() {return "showList";}
+    public ModelAndView showList() {
+        List<Shows> list = service.getAllShows();
+
+        return new ModelAndView("showList", "shows", list);
+    }
 
     @PostMapping("/save")
     public String addShow(@ModelAttribute Shows s) {
--- watchLog/src/main/java/com/watchLog/watchLog/service/ShowService.java
+++ watchLog/src/main/java/com/watchLog/watchLog/service/ShowService.java
@@ -5,6 +5,8 @@ import com.watchLog.watchLog.repository.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ShowService {
 
@@ -12,5 +14,9 @@ public class ShowService {
     private ShowRepository sRepo;
 
     public void save(Shows s) {sRepo.save(s);}
+
+    public List<Shows> getAllShows() {
+        return sRepo.findAll();
+    }
 
 }
