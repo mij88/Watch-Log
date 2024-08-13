3521e3946ff1ce96aaa46d87b06c0a0bd3b989d7
mij88 <mijalshrestha88@gmail.com>
2024-08-13 12:54:17 -0400

    Added edit button fro dropped shows and updated page style

--- watchLog/src/main/java/com/watchLog/watchLog/controller/DroppedShowListController.java
+++ watchLog/src/main/java/com/watchLog/watchLog/controller/DroppedShowListController.java
@@ -6,7 +6,10 @@ import com.watchLog.watchLog.service.DroppedShowService;
 import com.watchLog.watchLog.service.WatchedShowListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +20,12 @@ public class DroppedShowListController {
 
     @Autowired
     private WatchedShowListService watchedShowService;
+
+    @PostMapping("/saveDroppedShow")
+    public String addDroppedShow(@ModelAttribute DroppedShowList ds) {
+        service.saveDroppedShows(ds);
+        return "redirect:/dropped_shows";
+    }
 
     @RequestMapping("/deleteDroppedShow/{id}")
     public String deleteDroppedShow(@PathVariable("id") int id) {
@@ -34,5 +43,12 @@ public class DroppedShowListController {
 
        return "redirect:/dropped_shows";
 
+    }
+
+    @RequestMapping("/editDroppedShow/{id}")
+    public String editShow(@PathVariable("id") int id, Model model) {
+        DroppedShowList ds = service.getShowsById(id);
+        model.addAttribute("shows", ds);
+        return "DroppedShowEdit";
     }
 }
